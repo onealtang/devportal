6f73bb135e944416b1ba512a9fb53b477ea233db
onealtang <oneal.tang@gmail.com>
2012-09-04 23:45:13 +0800

    add code for search

---
+++ src/main/scala/code/snippet/Search.scala
@@ -0,0 +1,12 @@
+package snippet
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12-9-4
+ * Time: 下午11:34
+ * To change this template use File | Settings | File Templates.
+ */
+class Search {
+       def revision = "_" #> "test"
+}
